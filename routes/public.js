b5d7f7acb17f72561a48d19f1291be38bd2a01ca
GustaaSz <gugaabrantes2015@gmail.com>
2024-11-06 19:50:49 -0300

    adding the bcrypt to encrypt the password

--- routes/public.js
+++ routes/public.js
@@ -1,17 +1,44 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
 
 // Utilizar as rotas
 const router = express.Router()
 
 // Cadastro
-router.post('/cadastro', (req, res) => {
-    // req = request
-    // res = response
+// Usamos o async pra permitir o uso do await
+// -> req = request, -> res = response
 
-    const user = req.body // alocando em user os dados da requisição do body
+router.post('/cadastro', async (req, res) => {
+   
+    // try = caso dê errado...
+    try{
+        const user = req.body // alocando em user os dados da requisição do body
 
-    // Enviando uma resposta -> printando os dados de user
-    res.status(201).json(user) // status de criado com sucesso
+        const salt = await bcrypt.genSalt(10) // informando o peso da incriptação
+        // Alocando em hashPassword a senha que veio da requisição user e passando o peso salt
+        const hashPassword = await bcrypt.hash(user.password, salt);
+
+        // Await pede pra esperar pois irá receber uma resposta de um servidor, e não sabemos o time
+        const  userDB = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,   
+                password: hashPassword, // passando o hashPassword para o DB
+            },
+        })
+
+        // Enviando uma resposta -> printando os dados de user
+        res.status(201).json(userDB) // status de criado com sucesso
+
+        // Tratamos no catch
+    } catch(err){
+        res.status(500).json({ message: 'Erro no servidor, Tente novamente' })
+    }
+    
+
 })
 
 export default router;
