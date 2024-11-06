9ec2fe0678a7a62a19d62381f5ce5e92cd24df86
GustaaSz <gugaabrantes2015@gmail.com>
2024-11-06 20:36:00 -0300

    adding the LOGIN router

--- routes/public.js
+++ routes/public.js
@@ -7,10 +7,10 @@ const prisma = new PrismaClient()
 // Utilizar as rotas
 const router = express.Router()
 
-// Cadastro
 // Usamos o async pra permitir o uso do await
 // -> req = request, -> res = response
 
+// C A D A S T R O 
 router.post('/cadastro', async (req, res) => {
    
     // try = caso dê errado...
@@ -21,8 +21,9 @@ router.post('/cadastro', async (req, res) => {
         // Alocando em hashPassword a senha que veio da requisição user e passando o peso salt
         const hashPassword = await bcrypt.hash(user.password, salt);
 
-        // Await pede pra esperar pois irá receber uma resposta de um servidor, e não sabemos o time
-        const  userDB = await prisma.user.create({
+        // Await pede pra esperar
+        // const userDB = await prisma.user.create -> função para criar/cadastrar o user
+        const userDB = await prisma.user.create({
             data: {
                 email: user.email,
                 name: user.name,   
@@ -37,7 +38,32 @@ router.post('/cadastro', async (req, res) => {
     } catch(err){
         res.status(500).json({ message: 'Erro no servidor, Tente novamente' })
     }
-    
+})
+
+// L O G I N
+router.post('/login', async (req, res) => {
+
+    try{
+        const userInfo = req.body; // armazenando em userInfo os dados da requisição
+
+        // Verificando se o user existe
+        // findUnique -> busca um user 
+        // where: {email: userInfo.email} -> Busca pelo email que seja igual ao do userInfo.email
+        const user = await prisma.user.findUnique({
+            where: {email: userInfo.email} 
+        })
+
+        // Se não achar
+        if(!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado!' })
+        }
+
+        // Se achar
+        res.status(200).json(user)
+
+    } catch (err){
+        res.status(500).json({ message: 'Erro no servidor, Tente novamente' })
+    }
 
 })
 
