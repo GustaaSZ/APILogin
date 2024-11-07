e243150ceccafca86c702ab159867201ad616a94
GustaaSz <gugaabrantes2015@gmail.com>
2024-11-07 19:10:37 -0300

    enabling cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express'
 import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
 import auth from './middlewares/auth.js'
+import cors from 'cors'
 
 // Alocando o expresse em app
 const app = express()
 app.use(express.json())
+app.use(cors()) // -> Habilitando o cors e o acesso de qualqer página ao servidor
 
 app.use('/', publicRoutes) // tendo acesso a todas as rotas do publicRoutes
 app.use('/', auth, privateRoutes) // tendo acesso a todas as rotas do privateRoutes
